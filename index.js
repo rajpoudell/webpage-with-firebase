0dc32417b816a0cdda21461c38a0b8d1a82a2167
Raj Poudel <raj.poudeel@gmail.com>
2023-05-04 21:59:13 +0545

    refactoring function 44.19

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase , ref ,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+
 const appSettings = {
-    databaseURL :"https://first-website-using-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/"
-    
+databaseURL :"https://real-time-database-14c50-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
 }
+const app = initializeApp(appSettings)
+const database = getDatabase(app);
+const shoppingListInDB = ref(database , "shoppingList") //(location , what the ref called(name) )
 
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const parentEl = document.getElementById("shopping-list") //parent child
 
 addButtonEl.addEventListener("click",function () {
     let inputValue = inputFieldEl.value
-    console.log(inputValue);
+
+    
+    push(shoppingListInDB,inputValue ) //ref first argu and value 2nd argu
+
+    clearInputFieldEl();
+    appendItemto(inputValue) ;
+
+    console.log(`${inputValue} added to database`);
 })
+
+function clearInputFieldEl(){
+       inputFieldEl.value="";
+}
+function appendItemto(itemvalue) {
+    parentEl.innerHTML += `<li>${itemvalue}</li>`
+}
