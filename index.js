0b2d6f38cb61d3f20c61d0e19ea0d0bd3c91f10f
Raj Poudel <raj.poudeel@gmail.com>
2023-05-07 17:41:20 +0545

    making deleteitem process with js directly to the fire database

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ addButtonEl.addEventListener("click",function () {
     push(shoppingListInDB,inputValue ) //ref first argu and value 2nd argu
 
     clearInputFieldEl();
+    s
     // appendItemto(inputValue) ;  from commenting this line we prevent double fetching and adding element bug in while add to parentEl
 
     console.log(` ${inputValue} added to database`);
@@ -29,14 +30,28 @@ addButtonEl.addEventListener("click",function () {
 
 // fetching from database 
 onValue(shoppingListInDB , function(snapshot){
-    let  itemsArray = Object.values(snapshot.val())
-    parentEl.innerHTML = ""
+    // let  itemsArray = Object.values(snapshot.val())
+
+    let  itemsArray = Object.entries(snapshot.val()) //getting both unique id and value of data from database
+
+    // console.log(itemsArray)
+
+    clearShoppingListEl();
     for (let i = 0; i < itemsArray.length; i++) {
-        appendItemto(itemsArray[i])
+
+        let currentItem = itemsArray[i]; //storing data in currentItem converting into array
+        let currentItemID = currentItem[0];
+        let currentItemValue = currentItem[1];
+
+        appendItemto(currentItemValue) //appending value into parent element
         
     }
     
 })
+
+function clearShoppingListEl(){
+    parentEl.innerHTML="";
+}
 
 function clearInputFieldEl(){
        inputFieldEl.value="";
